4ff700c9913608703231cc2e27c7a8c01b1fa7d5
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-03-18 14:24:02 -0400

    Create Merge.java added pseudocode

---
+++ Merge.java
@@ -0,0 +1,19 @@
+
+class Merge{
+
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){}
+
+
+
+/* PSEUDOCODE
+mergesort(data,lo,hi):
+  if lo >= hi : 
+    return 
+  mergesort left side
+  mergesort right side
+  merge
+*/
+
+
+}
