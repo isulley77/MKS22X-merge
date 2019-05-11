0e0cec4f652fe884faf7346da2d4acf076da7ef4
isulley77 <isulley@stuy.edu>
2019-05-11 19:13:28 -0400

    added insertion sort()

--- Merge.java
+++ Merge.java
@@ -1,8 +1,19 @@
+import java.util.*;
+import java.io.*;
 
-class Merge{
+
+
+public class Merge{
 
   /*sort the array from least to greatest value. This is a wrapper function*/
-  public static void mergesort(int[]data){}
+  public static void mergesort(int[]data){
+  
+  
+  
+  
+  
+  
+  }
 
 
 
@@ -17,7 +28,20 @@ mergesort(data,lo,hi):
   
   
   
-  public void insertionsort(int[] data, int lo,int hi){}  
+  public void insertionsort(int[] arr){
+    int iter, holder, iter2;
+    
+    for(iter = 1;iter < arr.length; iter++){
+        holder = arr[iter];
+        iter2 = iter - 1;
+        
+        while(iter2 >= 0 && arr[iter2] > holder){
+            arr[iter2 + 1] = arr[iter2];
+            iter2--;
+        }
+        arr[iter2 + 1] = holder;
+    }
+  }  
 
 
 
